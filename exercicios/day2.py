cc7f1b9d1c89a8f1d51579214295591b776eb8a8
GuilhermeHR <guilhermehuertar@gmail.com>
2025-01-23 16:43:41 -0300

    Exercicios: Dia 2

---
+++ exercicios/day2.py
@@ -0,0 +1,42 @@
+# name = 'Guilherme'
+# age = 20
+# height = 1.74
+# student = True
+
+# print(name)
+# print(age)
+# print(height)
+# print(student)
+
+# born_year = 2023 - age
+# print("Ano de nascimento:", born_year)
+
+# heigher_age = age >= 18
+# print('Maior de idade?', heigher_age)
+
+# phrase = 'Olá! Meu nome é ' + name + ' e eu tenho ' + str(age) + ' anos.'
+# print(phrase)
+
+# number1 = float(input('Digite um numero:'))
+# number2 = float(input('Digite mais um numero:'))
+
+# sum = number1 + number2
+# subtraction = number1 - number2
+# multiplication = number1 * number2
+# division = number1 / number2
+
+# print('Soma:', sum)
+# print('Subtração:', subtraction)
+# print('Multiplicação:', multiplication)
+# print('Divisão:', division)
+
+# celsius = float(input('Digite uma temperatura graus em Celsius:'))
+# fahrenheit = celsius * 9 / 5 + 32
+# print(celsius, 'graus Celsius em Fahrenheit é:', fahrenheit)
+
+radius = float(input('Informe o raio do circulo desejado:'))
+
+pi = 3.14159
+area = pi * (radius ** 2)
+
+print('A área do circulo é de:', area)
