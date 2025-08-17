ce8b6b8142472bb1313d401fd05148de304cedc6
GuilhermeHR <guilhermehuertar@gmail.com>
2025-08-17 18:41:17 -0300

    Relembrando Conteudo: Conversão de Graus

---
+++ relembrando_conteudo/conversao_graus.py
@@ -0,0 +1,6 @@
+#Celsius para Fahrenheit
+temperature_celcius = float(input('Insira a temperatura em Celcius, para converte-la em Fahrenheit: '))
+
+temperature_fahreneit = temperature_celcius * 9 / 5 + 32
+
+print(f'{temperature_celcius}C° é igual a {temperature_fahreneit}F°')
